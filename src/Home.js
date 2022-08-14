5edc1f4a23f90c74029b0664ec63f8635ba5109d
Seth David Gyimah <esec26@gmail.com>
2022-08-14 23:39:48 +0000

    fill home component with dummy

--- src/Home.js
+++ src/Home.js
@@ -19,7 +19,57 @@ function Home(){
                 User sign up.
                 Insight and Analytics to track search history etc.
 
+
+                ...
+                Search four your favorite fiction, non fiction, academic and research findings using this web application
+                It uses googleapis (google books) to filter a large database of published materials and return the first 
+                20 closest matching to your search querry.
+
+                To use it, simply type the title or description of the book you are seeking to find. Click on any of the results
+                in the left pane to read it's description, author, year published etc.
+
+                Upcoming Features:
+                Sort & filter component
+                User sign up.
+                Insight and Analytics to track search history etc.
+
+                ..Search four your favorite fiction, non fiction, academic and research findings using this web application
+                It uses googleapis (google books) to filter a large database of published materials and return the first 
+                20 closest matching to your search querry.
+
+                To use it, simply type the title or description of the book you are seeking to find. Click on any of the results
+                in the left pane to read it's description, author, year published etc.
+
+                Upcoming Features:
+                Sort & filter component
+                User sign up.
+                Insight and Analytics to track search history etc.
+
+                ..Search four your favorite fiction, non fiction, academic and research findings using this web application
+                It uses googleapis (google books) to filter a large database of published materials and return the first 
+                20 closest matching to your search querry.
+
+                To use it, simply type the title or description of the book you are seeking to find. Click on any of the results
+                in the left pane to read it's description, author, year published etc.
+
+                Upcoming Features:
+                Sort & filter component
+                User sign up.
+                Insight and Analytics to track search history etc.
+                ..
+                Search four your favorite fiction, non fiction, academic and research findings using this web application
+                It uses googleapis (google books) to filter a large database of published materials and return the first 
+                20 closest matching to your search querry.
+
+                To use it, simply type the title or description of the book you are seeking to find. Click on any of the results
+                in the left pane to read it's description, author, year published etc.
+
+                Upcoming Features:
+                Sort & filter component
+                User sign up.
+                Insight and Analytics to track search history etc.
             </article>
+
         </div>
     )
 }
